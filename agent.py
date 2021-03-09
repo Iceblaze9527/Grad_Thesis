00405786c0e986d1799f86bcefa85cb449e99e6c
Iceblaze9527 <iceblaze9527@MacBookPro.local>
2021-03-09 21:11:41 +0800

    space encoding test passed, agent class wip

--- agent.py
+++ agent.py
@@ -1,50 +1,61 @@
+import numpy as np
 import random
 
 class Agent:
-    def __init__(self, s_ext, s_int, acts, alpha = 0.4, gamma=0.9):
-        """
-        The Q-values will be stored in a dictionary. Each key will be of the format: ((x, y), a). 
-        params:
-            actions (list): A list of all the possible action values.
-            alpha (float): step size
-            gamma (float): discount factor
-        """
-
-        #self.s_ext = s_ext
-        #self.s_int = s_int
-        self.acts = acts
+    def __init__(self, state_space, action_space, alpha = 0.5, gamma=0.9, epsilon = 0.1):
+        self.q_table = []## Q-table (state_space, action_space) ##random assigned value (bounded)
 
         self.alpha = alpha
         self.gamma = gamma
+        self.epsilon = epsilon
+        
+    def get_q_value(self, state, action):
+        return self.q_table[state][action]
 
-        self.Q = {}##Q-table
-
-    def get_Q_value(self, s_ext, s_int, action):##
+    def greedy_action_selection(self, state):
         """
-        Get q value for a state action pair.
-        params:
-            state (tuple): (x, y) coords in the grid
-            action (int): an integer for the action
+        Selects action with the highest Q-value for the given state.
         """
-        pass
-        #return self.Q.get((state, action), 0.0) # Return 0.0 if state-action pair does not exist
+        # Get all the Q-values for all possible actions for the state
+        q_values = [self.get_Q_value(state, action) for action in self.acts]
+        maxQ = max(q_values)
+        # There might be cases where there are multiple actions with the same high q_value. Choose randomly then
+        count_maxQ = q_values.count(maxQ)
+        if count_maxQ > 1:
+            # Get all the actions with the maxQ
+            best_action_indexes = [i for i in range(len(self.acts)) if q_values[i] == maxQ]
+            action_index = random.choice(best_action_indexes)
+        else:
+            action_index = q_values.index(maxQ)
+            
+        return self.actions[action_index]
 
-    def take_action(self, s_ext, s_int, epsilon=0.1):# epsilon-greedy
+    def action_probs(self, state):
+        """
+        Returns the probability of taking each action in the next state.
+        """
+        next_state_probs = [self.epsilon/len(self.acts)] * len(self.acts)
+        best_action = self.greedy_action_selection(state)
+        next_state_probs[best_action] += (1.0 - self.epsilon)
+
+        return next_state_probs
+
+    def take_action(self, state):# epsilon-greedy
         # Choose a random action
-        if random.random() < epsilon:
+        if random.random() < self.epsilon:
             action = random.choice(self.acts)
         # Choose the greedy action
         else:
-            action = self.greedy_action_selection(state)##s_ext x s_int
+            action = self.greedy_action_selection(state)
         
         return action
 
-    def learn(self, s_ext_old, s_int_old, s_ext_new, s_int_new, action_old, reward, epsilon):
+    def learn(self, state_old, state_new, action_old, reward):
         """
         Expected Sarsa update
         """
-        next_state_probs = self.action_probs(next_state, epsilon) # Probability for taking each action in next state
-        q_next_state = [self.get_Q_value(next_state, action) for action in self.actions] # Q-values for each action in next state
+        next_state_probs = self.action_probs(next_state, self.epsilon) # Probability for taking each action in next state
+        q_next_state = [self.get_Q_value(next_state, action) for action in self.acts] # Q-values for each action in next state
         next_state_expectation = sum([a*b for a, b in zip(next_state_probs, q_next_state)])
 
         q_current = self.Q.get((state, action), None) # If this is the first time the state action pair is encountered
@@ -52,31 +63,3 @@ class Agent:
             self.Q[(state, action)] = reward
         else:
             self.Q[(state, action)] = q_current + (self.alpha * (reward + self.gamma * next_state_expectation - q_current))
-
-    def greedy_action_selection(self, state):
-        """
-        Selects action with the highest Q-value for the given state.
-        """
-        # Get all the Q-values for all possible actions for the state
-        q_values = [self.get_Q_value(state, action) for action in self.actions]
-        maxQ = max(q_values)
-        # There might be cases where there are multiple actions with the same high q_value. Choose randomly then
-        count_maxQ = q_values.count(maxQ)
-        if count_maxQ > 1:
-            # Get all the actions with the maxQ
-            best_action_indexes = [i for i in range(len(self.actions)) if q_values[i] == maxQ]
-            action_index = random.choice(best_action_indexes)
-        else:
-            action_index = q_values.index(maxQ)
-            
-        return self.actions[action_index]
-
-    def action_probs(self, state, epsilon):
-        """
-        Returns the probability of taking each action in the next state.
-        """
-        next_state_probs = [epsilon/len(self.actions)] * len(self.actions)
-        best_action = self.greedy_action_selection(state)
-        next_state_probs[best_action] += (1.0 - epsilon)
-
-        return next_state_probs
--- main.py
+++ main.py
@@ -1,36 +1,32 @@
 import time
 
+from itertools import product
+
 from env import IntEnv
 from agent import Agent
 from logger import CustomLogger
 
-# TODO:
-# 1. encode state and action space
-# 2. specify internal env params
+## helper funcs
+def get_ext_state_space(ext_state_vars):
+    add_prefix = list(map(lambda var: [''.join(('DSB_',var)), ''.join(('ENV_',var))], ext_state_vars))
+    state_product = list(product(add_prefix[1], add_prefix[0]))## for more external states
+    ext_state_spaces = list(map(lambda item: ' & '.join((item[0], item[1])), state_product))## for more external states
+    return ext_state_spaces
 
 ## Main Params
 INPUT_PROMPT = "Input external states, 1 for 'ENABLED', 0 for 'DISABLED'."
 
-SUCCESS = 0
-FAIL = 1
-
-HAP_LOOK = 3
-SAD_LOOK = 4
-FEA_LOOK = 5
-ANG_LOOK = 6
-
 ## Agent Params
-ext_state_space = ['FOOD', 'TAIL']## Kronecker Product
-int_state_space = ['HAPPY', 'SAD', 'FEARFUL', 'ANGRY']## Kronecker Product
-action_space = {
-    'HAP_LOOK':HAP_LOOK, 
-    'SAD_LOOK':SAD_LOOK, 
-    'FEA_LOOK':FEA_LOOK, 
-    'ANG_LOOK':ANG_LOOK}
+ext_state_vars = ['FOOD', 'TAIL']
+ext_state_space = get_ext_state_space(ext_state_vars)# index == flag
+int_state_space = ['HAPPY', 'SAD', 'FEARFUL', 'ANGRY']# index == flag
+action_space = ['HAP_LOOK', 'SAD_LOOK', 'FEA_LOOK', 'ANG_LOOK']# index == flag
+SUCCESS = 5
+FAIL = 7
 
-alpha = 0.4
-gamma = 0.9
-epsilon = 0.15
+alpha = 0.5 # in [0,1], low if former knowledge is valued more, else new knowledge
+gamma = 0.9 # in (0,1), can neither be too low (not convergent) nor too high (for small state space)
+epsilon = 0.1 # can decline over time. Soft action selection policies include epsilon-greedy, epsilon-soft and softmax
 
 ## IntEnv Params
 ## HomeoSys Params
@@ -49,10 +45,8 @@ WB_MAX = 100
 WB_MIN = 0
 ## weight params
 
-
 ## control system
 def get_ext_states(prompt):
-    ## TODO: concurrent control: multi_threading
     ext_states_str = input(prompt)
     
     ext_states_lst = list(ext_states_str)
@@ -61,77 +55,108 @@ def get_ext_states(prompt):
     if not len(ext_states_lst) == len(ext_state_space):
         raise ValueError('Mismatched number of external states. External state num %d, input state num %d.'%(len(ext_state_space), len(ext_states_lst)))
     
-    ext_states = map(int, ext_states_lst)
+    ext_states_lst = list(map(int, ext_states_lst))
+    ext_states = sum([state << index for index, state in enumerate(ext_states_lst)])## compute flag index
+    
     return ext_states
 
 def send_action_cmd(action):
     assert (action in [SUCCESS, FAIL])|(action in action_space), "Illegal action flag."
 
-
 if __name__=='__main__':
+    state = lambda int_state, ext_states: int_state << len(ext_state_vars) + ext_states
     ## Init Environment and Agent
     # TODO: 
     # 1. proper input param lists
     # 2. proper initialization
     agent = Agent(
-        s_ext = ext_state_space, 
-        s_int = int_state_space, 
-        acts = action_space,
+        state_space = len(ext_state_space) * len(int_state_space),
+        action_space = len(action_space),
         alpha = alpha,
-        gamma = gamma)
+        gamma = gamma,
+        epsilon = epsilon)
     
-    int_env = IntEnv(homeo_vars)## 1. input params
+    int_env = IntEnv(
+        homeo_vars = homeo_vars)
     
-    log = CustomLogger('test.log')
+    log = CustomLogger('test.log')## start signal
     
+    ## Start Training
     cnt = 0
     t0 = time.perf_counter()
 
-    ext_states_old = get_ext_states(INPUT_PROMPT)
-    int_state_old, well_being = int_env.update(ext_states_old, cnt)
-    
-    action_old = agent.take_action(ext_states_old, int_state_old)
+    ext_states_old = get_ext_states(INPUT_PROMPT)# return flag
+    int_state_old, well_being = int_env.update(ext_states_old, cnt)# return flag (index)
+    action_old = agent.take_action(state(int_state_old, ext_states_old))# return flag (index)
     wb_prev = well_being
 
-    log.step_log(cnt=cnt, s_ext=ext_states_old, s_int=int_state_old, wb=well_being, reward=None, action=action_old, t_st = t0)
+    log.step_log(
+        cnt=cnt, 
+        s_ext=ext_state_space[ext_states_old],
+        s_int=int_state_space[int_state_old], 
+        wb=well_being, 
+        reward=None, 
+        action=action_old, 
+        t_st = t0)
 
     while True:
         cnt += 1
         t_start = time.perf_counter()
 
-        ext_states_new = get_ext_states(INPUT_PROMPT)
-        int_state_new, well_being = int_env.update(ext_states_new, cnt)##
+        ext_states_new = get_ext_states(INPUT_PROMPT)# return flag
+        int_state_new, well_being = int_env.update(ext_states_new, cnt)# return flag (index)
         
-        if well_being == WB_MAX:
-            ## TODO: concurrent control: multi_threading
-            ## reward = None, action = SUCCESS
+        if well_being == WB_MAX:## Halt
             send_action_cmd(SUCCESS)
             
-            log.step_log(cnt=cnt, s_ext=ext_states_new, s_int=int_state_new, wb=well_being, reward=None, action='SUCCESS', t_st=t_start)
+            log.step_log(
+                cnt=cnt, 
+                s_ext=ext_state_space[ext_states_new],
+                s_int=int_state_space[int_state_new], 
+                wb=well_being, 
+                reward=None, 
+                action='SUCCESS', 
+                t_st=t_start)
+            
             break
         else:
-            if well_being == WB_MIN:
-                ## TODO: concurrent control: multi_threading
-                ## reward = None, action = FAIL
+            if well_being == WB_MIN:## Halt
                 send_action_cmd(FAIL)
                 
-                log.step_log(cnt=cnt, s_ext=ext_states_new, s_int=int_state_new, wb=well_being, reward=None, action='FAIL', t_st=t_start)
+                log.step_log(
+                    cnt=cnt, 
+                    s_ext=ext_state_space[ext_states_new],
+                    s_int=int_state_space[int_state_new], 
+                    wb=well_being, 
+                    reward=None, 
+                    action='FAIL', 
+                    t_st=t_start)
                 break
             else:
                 reward = well_being - wb_prev
                 wb_prev = well_being
-                ## TODO: concurrent control: multi_threading
-                action_new = agent.take_action(ext_states_new, int_state_new)
+                action_new = agent.take_action(state(int_state_new, ext_states_new))# return flag (index)
                 send_action_cmd(action_new)
                 
-                agent.learn(ext_states_old, int_state_old, ext_states_new, int_state_new, action_old, reward, epsilon)
+                agent.learn(
+                    state(int_state_old, ext_states_old), 
+                    state(int_state_new, ext_states_new), 
+                    action_old, 
+                    reward)
                 
-                log.step_log(cnt=cnt, s_ext=ext_states_new, s_int=int_state_new, wb=well_being, reward=reward, action=action_new, t_st=t_start)
+                log.step_log(
+                    cnt=cnt, 
+                    s_ext=ext_state_space[ext_states_new],
+                    s_int=int_state_space[int_state_new], 
+                    wb=well_being, 
+                    reward=reward, 
+                    action=action_new, 
+                    t_st=t_start)
 
                 ext_states_old = ext_states_new
                 int_state_old = int_state_new
                 action_old = action_new
-
+    ## Terminate Training
     log.term_log(t0)
 
 # END_OF_FILE
