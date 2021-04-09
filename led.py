fd94e3b7144d11ae31408622136b86747bfc0525
pi <leona.xiang97@gmail.com>
2021-04-09 11:08:05 +0100

    fear eyes test passed

--- led.py
+++ led.py
@@ -3,6 +3,9 @@ import random
 from rpi_ws281x import Adafruit_NeoPixel, Color
 
 LED_COUNT      = 25      # Number of LED pixels.
+LED_ROW        = 5       # Row of LED pixels
+LED_COL        = 5       # Column of LED pixels
+
 LED_PIN        = 18      # GPIO pin connected to the pixels (must support PWM!).
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
@@ -26,28 +29,59 @@ def sad_eyes(strip_inst, steps):
 		
 		_rgb = int((_red << 16) | (_green << 8) | _blue)
 		
-		for i in range(0, num):
+		for i in range(num):
 			strip_inst.setPixelColor(i, _rgb)
 		
-		strip_inst.show()##
+		strip_inst.show()
 		time.sleep(interval)
 
+
+def fear_eyes(strip_inst, steps, diff, width):
+	jitt = lambda row, low_lst: diff if row in low_lst else 0
+
+	red_func = lambda step, jitt: 0xff - jitt
+	green_func = lambda step, jitt: 0xff - jitt
+	blue_func = lambda step, jitt: 0x00
+
+	n = 4 * width
+	low_lst = range(2 * width)
+	interval = PERIOD / steps
+	
+	for step in range(steps):
+		if (step % n) < (2 * width):
+			low_lst = [(row - 1) for row in low_lst]
+			low_lst = [(LED_ROW - 1) if row == -1 else row for row in low_lst]
+		else:
+			low_lst = [(row + 1) for row in low_lst]
+			low_lst = [0 if row == LED_ROW else row for row in low_lst]
+
+		for i in range(LED_ROW):
+			_red = red_func(step, jitt(i, low_lst))
+			_green = green_func(step, jitt(i, low_lst))
+			_blue = blue_func(step, jitt(i, low_lst))
+			
+			_rgb = int((_red << 16) | (_green << 8) | _blue)
+			
+			for j in range(LED_COL):
+				strip_inst.setPixelColor(j+5*i, _rgb)
+		
+		strip_inst.show()
+		time.sleep(interval)
 
 strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
 
 try:
-	# while True:
 	strip.begin()
-	sad_eyes(strip, 223)
+	# sad_eyes(strip, 223)
+	fear_eyes(strip, 80, 192, 1)
+	strip._cleanup()
 except KeyboardInterrupt:
+	strip._cleanup()
 	exit()
 	
 	
 	# #Reverse order	
-	# for i in range(0,strip.numPixels()):
-	# 	strip.setPixelColor(strip.numPixels()-i, Color(0,255,0))	
-	# 	strip.show()
-	# 	time.sleep(0.05)
+
 	# #Turn around
 	# for i in range(0,strip.numPixels()//4):
 	# 	strip.setPixelColor(i, Color(255,0,0))
