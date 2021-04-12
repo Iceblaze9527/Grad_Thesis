5b011d31bfc46764d6d5bc7390ba607461a5858c
pi <leona.xiang97@gmail.com>
2021-04-12 16:36:04 +0800

    input test completed

--- input.py
+++ input.py
@@ -1,24 +1,34 @@
 import time
 
+import numpy as np
+
+import smbus
 from gpiozero import Button
 import RPi.GPIO as GPIO
-import smbus
 
+ext_state_vars = ['FOOD', 'TOXIN', 'BOOP', 'PULL']
+
+#FOOD: BUTTON
 BUTTON_RED = 27
+#TOXIN: BUTTON
 BUTTON_YEL = 22
+#BOOP: TOUCH SENSOR
 TOUCH = 23
-
+#PULL: JOYSTICK
 ADDR = 0x48
 AIN0 = 0x00
 AIN1 = 0x01
+THRESHOLD = 192
 
-# ctrl.get_ext_states() in rl branch: get the external state code when called
-# default input values are all set to False(0)
-# within a time limit (e.g. 1s), scan and sample all input ports (we can get hundreds approx.)
-# check effective inputs (i.e. filter illegal inputs) (criteria? time costs?)
-# return input values when exceeding time limits (countdown methods?)
+TIME_LIMIT = 2#secs
+SAMPLE_INTERVAL = 0.04#sec
+MIN_EFF_INPUT_LEN = 5#samples
 
-if __name__ == '__main__':
+def get_ext_states():
+    t0 = time.time()
+    ## init
+    ext_state_samples = np.zeros(1, len(ext_state_vars), dtype=np.uint8)
+
     button_red = Button(pin=BUTTON_RED, pull_up=True)
     button_yel = Button(pin=BUTTON_YEL, pull_up=True)
     
@@ -29,23 +39,17 @@ if __name__ == '__main__':
     bus = smbus.SMBus(1)
     bus.write_byte(ADDR,AIN0)
     bus.read_byte(ADDR)#
-   
-    try:
-        while True:
-            print('red:', button_red.is_pressed)
-            print('yellow:', button_yel.is_pressed)
-            
-            print('touch:', GPIO.input(TOUCH))
-            
-            bus.write_byte(ADDR,AIN0)
-            x_val = bus.read_byte(ADDR)
-            print('joy_x:', x_val)
+    
+    while (time.time() - t0 < TIME_LIMIT):
+        sample = np.array([button_red.value, button_yel.value, GPIO.input(TOUCH), \
+            1 if bus.read_byte(ADDR) > THRESHOLD else 0]).reshape(1,-1)
+        ext_state_samples = np.concatenate((ext_state_samples, sample), axis=0)
 
-            bus.write_byte(ADDR,AIN1)
-            y_val = bus.read_byte(ADDR)
-            print('joy_y:', y_val)
-            
-            time.sleep(1)
-    except KeyboardInterrupt:
-        GPIO.cleanup(TOUCH)
-        exit()
+        time.sleep(SAMPLE_INTERVAL)
+    
+    ext_states_lst = np.where(np.count_nonzero(ext_state_samples, axis=0) > MIN_EFF_INPUT_LEN, 1, 0)
+    ext_states = sum([state << index for index, state in enumerate(ext_states_lst)])#return
+    
+    GPIO.cleanup(TOUCH)
+    
+    return ext_states
