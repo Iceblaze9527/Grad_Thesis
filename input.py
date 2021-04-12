403cb6b00f8a28beec9f3ce5ddd7b2a82e1dd325
pi <leona.xiang97@gmail.com>
2021-04-12 22:02:13 +0800

    input bug fixed

--- input.py
+++ input.py
@@ -11,7 +11,7 @@ ext_state_vars = ['FOOD', 'TOXIN', 'BOOP', 'PULL']
 #FOOD: BUTTON
 BUTTON_RED = 27
 #TOXIN: BUTTON
-BUTTON_YEL = 22
+BUTTON_YEL = 25
 #BOOP: TOUCH SENSOR
 TOUCH = 23
 #PULL: JOYSTICK
@@ -25,20 +25,20 @@ SAMPLE_INTERVAL = 0.04#sec
 MIN_EFF_INPUT_LEN = 5#samples
 
 def get_ext_states():
-    t0 = time.time()
-    ## init
-    ext_state_samples = np.zeros(1, len(ext_state_vars), dtype=np.uint8)
-
+    ##TODO: init (class needed)
     button_red = Button(pin=BUTTON_RED, pull_up=True)
     button_yel = Button(pin=BUTTON_YEL, pull_up=True)
-    
+
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
     GPIO.setup(TOUCH, GPIO.IN)
-    
+
     bus = smbus.SMBus(1)
     bus.write_byte(ADDR,AIN0)
     bus.read_byte(ADDR)#
+
+    t0 = time.time()
+    ext_state_samples = np.zeros((1, len(ext_state_vars)), dtype=np.uint8)
     
     while (time.time() - t0 < TIME_LIMIT):
         sample = np.array([button_red.value, button_yel.value, GPIO.input(TOUCH), \
@@ -46,10 +46,36 @@ def get_ext_states():
         ext_state_samples = np.concatenate((ext_state_samples, sample), axis=0)
 
         time.sleep(SAMPLE_INTERVAL)
+
+    ext_states_lst = []
+    for col in range(len(ext_state_vars)):
+        samples = ext_state_samples[1:,col]
+        differ = np.diff(samples)
+        pulse = np.flatnonzero(differ)
+        
+        if len(pulse) == 0:
+            ext_states_lst.append(1 if samples[-1] == 1 else 0)
+            continue
+        
+        voltage = np.split(samples, (pulse + 1))
+        high_cnt = map(lambda x:len(x), voltage[1::2] if differ[pulse[0]] == 1 else voltage[::2])
+        ext_states_lst.append(1 if max(high_cnt) >= MIN_EFF_INPUT_LEN else 0)
     
-    ext_states_lst = np.where(np.count_nonzero(ext_state_samples, axis=0) > MIN_EFF_INPUT_LEN, 1, 0)
-    ext_states = sum([state << index for index, state in enumerate(ext_states_lst)])#return
-    
+    ext_states = sum([state << index for index, state in enumerate(ext_states_lst)])
+
+    #TODO: close and clean (class methods)
+    button_red.close()
+    button_yel.close()
     GPIO.cleanup(TOUCH)
+    bus.close()
     
     return ext_states
+
+if __name__ == '__main__':
+    try:
+        while True:
+            x = get_ext_states()
+            print(x)
+            time.sleep(0.05)
+    except KeyboardInterrupt:
+        exit()
--- output.py
+++ output.py
@@ -1,13 +1,16 @@
 from led import LEDEyes
 from sound import SoundPlay
 
+int_state_space = ['HAPPY', 'SAD', 'FEAR', 'ANGRY']
+
 LED_ROW        = 5       # Row of LED pixels
 LED_COL        = 5       # Column of LED pixels
-LED_PIN        = 18      # GPIO pin connected to the pixels (must support PWM!).
+LED_PIN        = 12      # GPIO pin connected to the pixels (must support PWM!).
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
 LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
-LED_BRIGHTNESS = 15    # Set to 0 for darkest and 255 for brightest
+LED_BRIGHTNESS = 15      # Set to 0 for darkest and 255 for brightest
+LED_CHANNEL    = 0       # PWM channel index
 
 PERIOD = 3 #seconds
 
@@ -20,15 +23,16 @@ WAV_ANGRY = 'angry-1.wav'
 # ctrl.send_action_signal() in rl branch: get action signal and acts accordingly
 # init and if-else
 # done within 3 secs
-# success and fail cmd?
+# ready, success and fail cmd?
+# _cleanup() method
 
-if __name__ == '__main__':
+def send_action_command(action):
     sound_happy = SoundPlay(PATH, WAV_HAPPY)
     sound_sad = SoundPlay(PATH, WAV_SAD)
     sound_fear = SoundPlay(PATH, WAV_FEAR)
     sound_angry = SoundPlay(PATH, WAV_ANGRY)
     
-    led = LEDEyes(LED_ROW, LED_COL, PERIOD, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
+    led = LEDEyes(LED_ROW, LED_COL, PERIOD, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
     
     try:
         sound_happy.play()
@@ -48,3 +52,5 @@ if __name__ == '__main__':
         sound_angry.stop()
     except KeyboardInterrupt:
         exit()
+
+send_action_command(0)
